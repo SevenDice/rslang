bd557e51cd8a5caa72326876f70dc8f34577c074
sevendc <benford.mark783@gmail.com>
2020-06-14 21:17:54 +0400

    feat: change title to users

--- src/components/profiles/Profiles.js
+++ src/components/profiles/Profiles.js
@@ -16,10 +16,9 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
         <Spinner />
       ) : (
         <Fragment>
-          <h1 className='large text-primary'>Developers</h1>
+          <h1 className='large text-primary'>Users</h1>
           <p className='lead'>
-            <i className='fab fa-connectdevelop' /> Browse and connect with
-            developers
+            <i className='fab fa-connectdevelop' /> Browse users
           </p>
           <div className='profiles'>
             {profiles.length > 0 ? (
