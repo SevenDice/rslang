71f7c7326b4957fb8360ec52d6a9281ee9d9647a
Victoria Triputina <triputina.victoria@gmail.com>
2020-06-23 12:35:00 +0300

    refactor: rename icons from russian to english

--- src/components/layout/Burgermenue.js
+++ src/components/layout/Burgermenue.js
@@ -5,14 +5,14 @@ import ListItem from "./ListItem";
 const Burgermenue = () => {
 
     const navItems = [
-        {key: 'link-main', link : '#', image : 'главная.png', text : 'Главная'},
-        {key: 'link-learn',link : 'learn', image : 'учить слова.png', text : 'Учим слова'},
-        {key: 'link-games',link : 'games', image : 'игра1.png', text : 'Игры'},
-        {key: 'link-vocabulary',link : 'vocabulary', image : 'словарь.png', text : 'Словарь'},
-        {key: 'link-settings',link : 'settings', image : 'настройки.png', text : 'Настройки'},
-        {key: 'link-statistics',link : 'statistics', image : 'статистика.png', text : 'Статистика'},
-        {key: 'link-about',link : 'about', image : 'команда.png', text : 'О нас'},
-        {key: 'link-promopage',link : 'promopage', image : 'промо.png', text : 'Промо'}
+        {key: 'link-main', link : '#', image : 'main.png', text : 'Главная'},
+        {key: 'link-learn',link : 'learn', image : 'learn.png', text : 'Учим слова'},
+        {key: 'link-games',link : 'games', image : 'games1.png', text : 'Игры'},
+        {key: 'link-vocabulary',link : 'vocabulary', image : 'vocabulary.png', text : 'Словарь'},
+        {key: 'link-settings',link : 'settings', image : 'settings.png', text : 'Настройки'},
+        {key: 'link-statistics',link : 'statistics', image : 'statistics.png', text : 'Статистика'},
+        {key: 'link-about',link : 'about', image : 'about.png', text : 'О нас'},
+        {key: 'link-promopage',link : 'promopage', image : 'promopage.png', text : 'Промо'}
     ]
 
     return(
