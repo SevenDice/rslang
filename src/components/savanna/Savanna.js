994defd853cd9ee1f52ab445f99b5f3c64516dc0
Victoria Triputina <triputina.victoria@gmail.com>
2020-06-30 17:43:05 +0300

    feat: add basic layoyt, starting function and counting down timer

--- src/components/savanna/Savanna.js
+++ src/components/savanna/Savanna.js
@@ -1,11 +1,81 @@
-import React from 'react';
+import React from "react";
 
 const Savanna = () => {
+  const [gamingMode, setGamingMode] = React.useState({
+    isStarted: false,
+  });
+
+  const [countingDown, setCountingDown] = React.useState({
+    isTimer: false,
+  })
+
+  const [timer, setTimer] = React.useState({
+    timer: 3,
+  });
+
+  function toggleGamingMode() {
+    setGamingMode({
+      isStarted: !gamingMode.isStarted,
+    });
+    setCountingDown({
+      isTimer: !countingDown.timer,
+    })
+  }
+
+  function startGame(){
+    toggleGamingMode();
+    countDown()
+    setTimeout(() => {
+      // setCountingDown({
+      //   isTimer: !countingDown.timer,
+      // })
+    }, 5000)
+  }
+
+  function countDown() {
+    let clock = setInterval(() => {
+      setTimer({
+        timer: --timer.timer,
+      });
+    }, 1000);
+    setTimeout(() => {
+      clearInterval(clock);
+      setCountingDown({
+        isTimer: !countingDown.timer,
+      })
+    }, 3000);
+  }
+
   return (
-    <section class='wrapper style5'>
-      <div class='inner'>
-        <h1 className='large text-primary'>Здесь будет игра Саванна</h1>
-      </div>
+    <section className="wrapper style5 savanna">
+      {(gamingMode.isStarted === false ? (
+        <div className="inner">
+          <h1 className="landing-logo">САВАННА</h1>
+          <p>
+            Правила игры: за 5 секунд необходимо выбрать правильный вариант
+            ответа из четырех предложенных. Всего необходимо будет ответить на
+            30 вопросов. Ошибиться можно будет всего 5 раз за всю игру.
+          </p>
+          <button
+            className="primary"
+            id="button-start"
+            onClick={startGame}
+          >
+            Начать игру
+          </button>
+        </div>
+      ) : (
+        countingDown.isTimer ? (
+          <div className="inner">
+            <p className="counter">{timer.timer}</p>
+          </div>
+        ) : (
+          <div className="inner">
+            <p className="counter">{countingDown.isTimer}</p>
+          </div>
+        )
+      ))
+      }
     </section>
   );
 };
