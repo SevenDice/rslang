d6a8fcbabcdda13de076a913c845dd162749abf1
Alina Kutyeva <flytta.dig@mail.ru>
2020-07-06 01:20:50 +0300

    fix: prevent app crashing because of settings absence

--- src/components/speakIt/Speakit.js
+++ src/components/speakIt/Speakit.js
@@ -1,39 +1,61 @@
 import React from 'react';
 import SpeakitGame from './SpeakitGame';
 import { Link } from 'react-router-dom';
-const Speakit = () => {
+import Loading from './Loading';
+import { getUserSettings } from '../../actions/profile';
+import { connect } from 'react-redux';
+
+const Speakit = ({ getUserSettings, profile: { settings } }) => {
   const [isStarted, setIsStarted] = React.useState(false);
 
   const startHandler = () => {
     setIsStarted(!isStarted);
   };
 
+  React.useEffect(() => {
+    getUserSettings(localStorage.getItem('id'));
+  }, [getUserSettings]);
+
   return (
-    <div className='speakit-startpage'>
-      {' '}
-      <Link to='/dashboard'>
-        <div className='speakit-exit' title='Выйти на главную страницу' />
-      </Link>
-      {isStarted ? (
-        <SpeakitGame />
+    <div>
+      {settings === null ? (
+        <div className='speakit-startpage'>
+          <Loading />
+        </div>
       ) : (
-        <div className='speakit-decs'>
-          <p className='speakit-rules'>
-            Кликайте по словам, чтобы услышать их произношение.
-            <br /> Когда будете готовы, кликнете по кнопке "Начать говорить" и произносите слова.
-            <br /> Порядок произношения не важен.
-            <br />
-            Как только вы произнесете присутствующее слово, оно будет подсвечено.
-            <br /> Постарайтесь подсветить все слова.
-            <br /> Удачи :)
-          </p>
-          <button onClick={startHandler} className='button primary'>
-            Начать игру
-          </button>
+        <div className='speakit-startpage'>
+          {' '}
+          <Link to='/dashboard'>
+            <div className='speakit-exit' title='Выйти на главную страницу' />
+          </Link>
+          {isStarted ? (
+            <SpeakitGame />
+          ) : (
+            <div className='speakit-decs'>
+              <p className='speakit-rules'>
+                Кликайте по словам, чтобы услышать их произношение.
+                <br /> Когда будете готовы, кликнете по кнопке "Начать говорить" и произносите
+                слова.
+                <br /> Порядок произношения не важен.
+                <br />
+                Как только вы произнесете присутствующее слово, оно будет подсвечено.
+                <br /> Постарайтесь подсветить все слова.
+                <br /> Удачи :)
+              </p>
+              <button onClick={startHandler} className='button primary'>
+                Начать игру
+              </button>
+            </div>
+          )}
         </div>
       )}
     </div>
   );
 };
 
-export default Speakit;
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getUserSettings })(Speakit);
