5584c1bef1bd45f26ee6053672862503dd33bcf1
Angelina German <angyricci.art@gmail.com>
2020-07-12 20:05:18 +0300

    feat: add description for the game

--- src/components/audiocall/components/LevelSelector.js
+++ src/components/audiocall/components/LevelSelector.js
@@ -13,7 +13,9 @@ class LevelSelector extends Component {
   render() {
     return (
       <>
-        <h4>Выбор уровеня</h4>
+        <p>Ход игры: звучит произношение слова на английском языке, нужно выбрать перевод слова из пяти предложенных
+          вариантов ответа.</p>
+        <h4>Выбор уровня</h4>
         <select name="level" defaultValue={this.props.level} onChange={this.props.handleLevelChange}>
           {this.getList().map((value, index) => {
             return <option key={index} value={index}>{index}</option>
